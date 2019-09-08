918ca7a16628df17b0e1916f3595b57331d44cd5
baihacker <bailiangsky@gmail.com>
2019-09-08 14:57:59 -0700

    1. Change clang/mingw version and use clang 8.0.1.

--- setup.py
+++ setup.py
@@ -232,9 +232,7 @@ def setup_environment_variables():
 
   if util.IS_WIN:
     add_if_exists(
-        os.path.join(ROOTDIR, 'app\\DevSoft\\MinGW-x86_64_9.2.0-msys2\\bin'), dev_paths)
-    add_if_exists(
-        os.path.join(ROOTDIR, 'app\\DevSoft\\LLVM_7.0.0\\bin'), dev_paths)
+        os.path.join(ROOTDIR, 'app\\DevSoft\\LLVM_8.0.1\\bin'), dev_paths)
     add_if_exists(os.path.join(ROOTDIR, 'app\\MathsSoft\\mma'), dev_paths)
 
     add_if_exists('C:\\Python27', dev_paths)
@@ -251,6 +249,9 @@ def setup_environment_variables():
     add_if_exists('C:\\Program Files (x86)\\Vim\\vim81', dev_paths)
 
     add_if_exists('C:\\Program Files\\TortoiseSVN\\bin', dev_paths)
+
+    add_if_exists(
+        os.path.join(ROOTDIR, 'app\\DevSoft\\MinGW-x86_64_9.2.0-msys2\\bin'), dev_paths)
 
   if len(JAVAHOME) > 0:
     add_if_exists(os.path.join(JAVAHOME, 'bin'), dev_paths)
